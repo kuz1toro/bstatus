a7145f9a5b0921da501e333a5617bcecd007bb57
kuSwaNtoRo <kuz1toro@gmail.com>
2019-07-04 15:49:37 +0700

    04-07-2019;15:49

--- application/views/dinas/chart.php
+++ application/views/dinas/chart.php
@@ -75,6 +75,175 @@
                                 <td class="textright">
                                     16.73                                                            </td>
                             </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Rekomendasi Sertifikat Keselamatan Kebakaran (SKK)</td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    <a target="_blank" href="http://b-status.arkadana.com/xadmin/dashboard/table/D/SLF/all">
+                                    <?php echo $skk_pemda[0]; ?>
+                                    </a>
+                                </td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    0.54                                                            </td>
+                                <td class="textright">
+                                    <a target="_blank" href="http://b-status.arkadana.com/xadmin/dashboard/table/P/SLF/all">
+                                    <?php echo $skk_pusat[0]; ?>
+                                    </a>
+                                </td>
+                                <td class="textright">
+                                    1.29                                                            </td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    <a target="_blank" href="http://b-status.arkadana.com/xadmin/dashboard/table/S/SLF/all">
+                                    <?php echo $skk_swasta[0]; ?>
+                                    </a>
+                                </td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    14.9                                                            </td>
+                                <td class="textright">
+                                    156                                                            </td>
+                                <td class="textright">
+                                    16.73                                                            </td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Laporan Hasil Pemeriksaan (LHP) PLUS</td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    <a target="_blank" href="http://b-status.arkadana.com/xadmin/dashboard/table/D/SLF/all">
+                                    <?php echo $lhp_plus_pemda[0]; ?>
+                                    </a>
+                                </td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    0.54                                                            </td>
+                                <td class="textright">
+                                    <a target="_blank" href="http://b-status.arkadana.com/xadmin/dashboard/table/P/SLF/all">
+                                    <?php echo $lhp_plus_pusat[0]; ?>
+                                    </a>
+                                </td>
+                                <td class="textright">
+                                    1.29                                                            </td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    <a target="_blank" href="http://b-status.arkadana.com/xadmin/dashboard/table/S/SLF/all">
+                                    <?php echo $lhp_plus_swasta[0]; ?>
+                                    </a>
+                                </td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    14.9                                                            </td>
+                                <td class="textright">
+                                    156                                                            </td>
+                                <td class="textright">
+                                    16.73                                                            </td>
+                            </tr>
+                            <tr style="background-color: #E1F0F8; font-weight: bold;">
+                                <td colspan="2" class="text-right" style="padding-right: 15px;">SUB TOTAL:</td>
+                                <td class="textright">13</td>
+                                <td class="textright">1.38</td>
+                                <td class="textright">38</td>
+                                <td class="textright">4.04</td>
+                                <td class="textright">617</td>
+                                <td class="textright">65.64</td>
+                                <td class="textright">668</td>
+                                <td class="textright">71.06</td>
+                            </tr>
+                            <tr style="background-color: #ececec">
+                                <td>#</td>
+                                <td>Tidak Memenuhi Syarat Keselamatan Kebakaran</td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Laporan Hasil Pemeriksaan (LHP) MIN</td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    <a target="_blank"href="http://b-status.arkadana.com/xadmin/dashboard/table/D/LHP_min/all">
+                                    <?php echo $lhp_min_pemda[0]; ?>
+                                    </a>
+                                </td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    0.96                                                            </td>
+                                <td class="textright">
+                                    <a target="_blank"href="http://b-status.arkadana.com/xadmin/dashboard/table/P/LHP_min/all">
+                                    <?php echo $lhp_min_pusat[0]; ?>
+                                    </a>
+                                </td>
+                                <td class="textright">
+                                    2.87                                                            </td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    <a target="_blank"href="http://b-status.arkadana.com/xadmin/dashboard/table/S/LHP_min/all">
+                                    <?php echo $lhp_min_swasta[0]; ?>
+                                    </a>
+                                </td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    9.36                                                            </td>
+                                <td class="textright">
+                                    124                                                            </td>
+                                <td class="textright">
+                                    13.19                                                            </td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Surat Peringatan I (SP I)</td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    <a target="_blank"href="http://b-status.arkadana.com/xadmin/dashboard/table/D/LHP_min/all">
+                                    <?php echo $sp1_pemda[0]; ?>
+                                    </a>
+                                </td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    0.96                                                            </td>
+                                <td class="textright">
+                                    <a target="_blank"href="http://b-status.arkadana.com/xadmin/dashboard/table/P/LHP_min/all">
+                                    <?php echo $sp1_pusat[0]; ?>
+                                    </a>
+                                </td>
+                                <td class="textright">
+                                    2.87                                                            </td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    <a target="_blank"href="http://b-status.arkadana.com/xadmin/dashboard/table/S/LHP_min/all">
+                                    <?php echo $sp1_swasta[0]; ?>
+                                    </a>
+                                </td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    9.36                                                            </td>
+                                <td class="textright">
+                                    124                                                            </td>
+                                <td class="textright">
+                                    13.19                                                            </td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Surat Peringatan II (SP II)</td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    <a target="_blank"href="http://b-status.arkadana.com/xadmin/dashboard/table/D/LHP_min/all">
+                                    <?php echo $sp2_pemda[0]; ?>
+                                    </a>
+                                </td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    0.96                                                            </td>
+                                <td class="textright">
+                                    <a target="_blank"href="http://b-status.arkadana.com/xadmin/dashboard/table/P/LHP_min/all">
+                                    <?php echo $sp2_pusat[0]; ?>
+                                    </a>
+                                </td>
+                                <td class="textright">
+                                    2.87                                                            </td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    <a target="_blank"href="http://b-status.arkadana.com/xadmin/dashboard/table/S/LHP_min/all">
+                                    <?php echo $sp2_swasta[0]; ?>
+                                    </a>
+                                </td>
+                                <td class="textright" style="background-color: #F6F6F7;">
+                                    9.36                                                            </td>
+                                <td class="textright">
+                                    124                                                            </td>
+                                <td class="textright">
+                                    13.19                                                            </td>
+                            </tr>
+                            
                         </tbody>
                     </table>
                 </div>
