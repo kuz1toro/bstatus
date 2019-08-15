468c037271e05ed345ee419d2650f2c56a2c0427
kuSwaNtoRo <kuz1toro@gmail.com>
2019-08-15 15:59:33 +0700

    15-08-2019:15:59

--- application/libraries/Pdf.php
+++ application/libraries/Pdf.php
@@ -12,15 +12,18 @@ class Pdf extends TCPDF
         $image_file = FCPATH.'assets/img/logo_dki.png';
         $this->Image($image_file, 15, 10, 15, '', 'PNG', '', 'T', false, 300, '', false, false, 0, false, false, false);
         // Set font
-        $this->SetFont('times', '', 17);
+        $this->SetFont('times', 'B', 13);
 		// Title
 		//$this->Cell(0, 0, 'tes', 0, false, 'C', 0, '', 0, false, 'M', 'M');
 		$this->SetXY(30, 13);
 		$this->Cell(160, 0, 'PEMERINTAH PROVINSI DAERAH KHUSUS IBUKOTA JAKARTA', 1, false, 'C', 0, '', 1, false, 'M', 'M');
-        $this->SetFont('times', 'B', 17);
-		$this->SetXY(30, 21);
+        //$this->SetFont('times', 'B', 15);
+		$this->SetXY(30, 19);
 		$this->Cell(160, 0, 'DINAS PENANGGULANGAN KEBAKARAN DAN PENYELAMATAN', 1, false, 'C', 0, '', 1, false, 'M', 'M');
-		//$this->Cell(0, 0, '   tes', 1, false, 'C', 0, '', 1, false, 'M', 'M');
+        //$this->Cell(0, 0, '   tes', 1, false, 'C', 0, '', 1, false, 'M', 'M');
+        $this->SetFont('times', 'B', 10);
+        $this->SetXY(30, 24);
+		$this->Cell(160, 0, 'http://www.jakartafire.net', 1, false, 'C', 0, '', 1, false, 'M', 'M');
     }
 
     // Page footer
